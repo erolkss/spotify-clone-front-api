e34d508900c9c30ca14948a5c89f98fbd087c1f3
Lucas Lima <lucaserolima@gmail.com>
2025-07-09 12:11:51 -0400

    feat(toast): add toast notification system with autohide support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,29 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {fontAwesomeIcons} from './shared/font-awesome-icons';
 import {NavigationComponent} from './layout/navigation/navigation.component';
 import {LibraryComponent} from './layout/library/library.component';
 import {HeaderComponent} from './layout/header/header.component';
+import {ToastService} from '../service/toast.service';
+import {NgbToast} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent, HeaderComponent],
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent, HeaderComponent, NgbToast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spotify-clone-front';
 
   private faIconLibrary = inject(FaIconLibrary);
 
+  toastService = inject(ToastService);
+
   ngOnInit(): void {
     this.initFontAwesome();
+    this.toastService.show("Hello Toast", "SUCCESS");
   }
 
   private initFontAwesome() {
