5cbfe6df357bc9170b049a101e0bb7e1b87e33df
Lucas Lima <lucaserolima@gmail.com>
2025-07-10 16:01:33 -0400

    feat(app): enable Angular animations with provideAnimations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {provideAnimations} from "@angular/platform-browser/animations";
 import { routes } from './app.routes';
 import {provideHttpClient, withXsrfConfiguration} from '@angular/common/http';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
       withXsrfConfiguration({cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN'})
-    )
+    ),
+    provideAnimations()
   ]
 };
