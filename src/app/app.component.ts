986a8a8d4a8d9174751f0021c461c9b0278e9b56
Lucas Lima <lucaserolima@gmail.com>
2025-07-11 16:13:32 -0400

    feat(app): react to auth popup state in AppComponent using signal effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, effect, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {fontAwesomeIcons} from './shared/font-awesome-icons';
 import {NavigationComponent} from './layout/navigation/navigation.component';
 import {LibraryComponent} from './layout/library/library.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {ToastService} from '../service/toast.service';
-import {NgbToast} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, NgbModalRef, NgbToast} from '@ng-bootstrap/ng-bootstrap';
 import {PlayerComponent} from './layout/player/player.component';
+import {AuthPopupState, AuthService} from '../service/auth.service';
+import {AuthPopupComponent} from './layout/auth-popup/auth-popup.component';
 
 @Component({
   selector: 'app-root',
@@ -22,11 +24,51 @@ export class AppComponent implements OnInit {
 
   toastService = inject(ToastService);
 
+  private authService = inject(AuthService);
+
+  private modalService = inject(NgbModal);
+
+  private authModalRef: NgbModalRef | null = null;
+
+  constructor() {
+    effect(() => {
+      this.openOrCloseAuthModal(this.authService.authPopupStateChange())
+    });
+  }
+
   ngOnInit(): void {
     this.initFontAwesome();
   }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private openOrCloseAuthModal(state: AuthPopupState) {
+    if (state === "OPEN") {
+      this.openAuthPopup();
+    } else if (this.authModalRef !== null && state === "CLOSE"
+      && this.modalService.hasOpenModals()) {
+      this.authModalRef.close();
+    }
+  }
+
+  private openAuthPopup() {
+    this.authModalRef = this.modalService.open(AuthPopupComponent, {
+      ariaDescribedBy: 'authentication-modal',
+      centered: true
+    });
+
+    this.authModalRef.dismissed.subscribe({
+      next: () => {
+        this.authService.openOrCloseAuthPopup("CLOSE");
+      }
+    });
+
+    this.authModalRef.closed.subscribe({
+      next: () => {
+        this.authService.openOrCloseAuthPopup("CLOSE");
+      }
+    });
   }
 }
