6af2549f366af4a3200cae124f962a8c227a8661
Lucas Lima <lucaserolima@gmail.com>
2025-07-11 16:14:13 -0400

    chore(http): register AuthInterceptor in HttpClient configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideAnimations} from "@angular/platform-browser/animations";
 import { routes } from './app.routes';
-import {provideHttpClient, withXsrfConfiguration} from '@angular/common/http';
+import {provideHttpClient, withInterceptors, withXsrfConfiguration} from '@angular/common/http';
+import {authInterceptor} from './service/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
+      withInterceptors([authInterceptor]),
       withXsrfConfiguration({cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN'})
     ),
     provideAnimations()
